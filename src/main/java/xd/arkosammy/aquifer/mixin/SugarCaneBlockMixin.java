83f46de010983bc4c31fdcd9dcc929ec471a4452
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-01-08 03:00:17 -0500

    Removed unused import

--- src/main/java/xd/arkosammy/aquifer/mixin/SugarCaneBlockMixin.java
+++ src/main/java/xd/arkosammy/aquifer/mixin/SugarCaneBlockMixin.java
@@ -12,7 +12,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(SugarCaneBlock.class)
 public class SugarCaneBlockMixin {
